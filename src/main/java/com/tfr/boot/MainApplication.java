616f56c65bc374d5f4c003c4bff082d2ba760ac0
TangFangrun <578909881@qq.com>
2021-08-12 21:54:51 +0800

    测试的

---
+++ src/main/java/com/tfr/boot/MainApplication.java
@@ -0,0 +1,18 @@
+package com.tfr.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: rain
+ * @date: Created in 2020/12/29 19:32
+ * @version: 1.0
+ * @modified By:
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String args[]){
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
---
+++ src/main/java/com/tfr/boot/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.tfr.boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: rain
+ * @date: Created in 2020/12/29 19:34
+ * @version: 1.0
+ * @modified By:
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "hello world";
+    }
+}
